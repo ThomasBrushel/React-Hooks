65e487c76c62d4fd44434d3a833a5330c48c2a22
Thomas Brushel <thomas@wantable.com>
2019-02-07 19:26:51 -0600

    Implemented Search

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import axios from 'axios';
 
 function App(){
   const [data, setData] = useState({ hits: [] }); // Hits = Hacker News Articles...
+  const [query, setQuery] = useState('redux');
 
   const fetchData = async () => {
     const result = await axios(
-      'http://hn.algolia.com/api/v1/search?query=redux',
+      `http://hn.algolia.com/api/v1/search?query=${query}`,
       );
     setData(result.data);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [query]);
 
   return(
+    <React.Fragment>
+      <input type="text"
+             value={query}
+             onChange={event => setQuery(event.target.value)}
+      />
     <ul>
       {data.hits.map(item => (
         <li key={item.objectID}>
@@ -23,6 +29,7 @@ function App(){
         </li>
       ))}
     </ul>
+    </React.Fragment>
   );
 
 }
